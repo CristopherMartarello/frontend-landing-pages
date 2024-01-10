1fe20384a858d1d41e1405fff8c13a2131000d92
CristopherMartarello <crisrossi1313@gmail.com>
2024-01-10 12:23:48 -0300

    [Heading] Criação e estilização do componente heading

---
+++ src/styles/theme.js
@@ -0,0 +1,25 @@
+export const theme = {
+  colors: {
+    primaryColor: '#0A1128',
+    secondaryColor: '#dc143c',
+    white: '#FFFFFF',
+  },
+  font: {
+    family: {
+      dafault: "'Open Sans', sans-serif",
+      secondary: "'Montserrat', sans-serif",
+    },
+    sizes: {
+      xsmall: '8rem',
+      small: '1.6rem',
+      medium: '2.4rem',
+      large: '3.2rem',
+      xlarge: '4.0rem',
+      xxlarge: '4.8rem',
+      xhuge: '6.4rem',
+    },
+  },
+  media: {
+    lteMedium: '(max-width: 768px)',
+  },
+};
