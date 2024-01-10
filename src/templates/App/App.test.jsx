34179a80e658144e3833b7a2de1ea77a963ba007
CristopherMartarello <crisrossi1313@gmail.com>
2024-01-10 12:48:51 -0300

    [HeadingTests] Testes do componente Heading

--- src/templates/App/App.test.jsx
+++ src/templates/App/App.test.jsx
@@ -1,16 +1,9 @@
-import { screen } from '@testing-library/react';
 import Home from '.';
 import { theme } from '../../styles/theme';
 import { renderTheme } from '../../styles/renderTheme';
 
-test('renders learn react link', () => {
-  const { debug } = renderTheme(<Home />);
-  const headingContainer = screen.getByRole('heading', {
-    name: 'Hello',
-  }).parentElement;
-  expect(headingContainer).toHaveStyle({
-    background: theme.colors.mainBg,
+describe('<Home />', () => {
+  it('should render home', () => {
+    renderTheme(<Home />);
   });
-  expect(headingContainer).toMatchSnapshot();
-  expect(headingContainer).toHaveStyleRule('background', 'red');
 });
